f89f7f359065849edcd4ce7c7fbd530335b9df99
carlso70 <carlso70@purdue.edu>
2017-12-12 13:24:26 -0500

    Setup login component

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import Login from './pages/Login';
 import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 class App extends Component {
   render() {
     return (
+      <MuiThemeProvider>
       <div className="App">
             <Login />
       </div>
+      </MuiThemeProvider>
     );
   }
 }
--- webapp/src/pages/Login.js
+++ webapp/src/pages/Login.js
@@ -1,19 +1,49 @@
 import React, { Component } from 'react';
-import { render } from 'react-dom';
-import TextField from 'material-ui/Button';
+import TextField from 'material-ui/TextField';
+import FlatButton from 'material-ui/FlatButton';
+import Dialog from 'material-ui/Dialog';
 
 class Login extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            open: true
+        }
+    }
+
+    // Log in the user, and save token to localstorage
+    logIn() {
+    }
+
+    close = () => {
+        this.setState({
+            open: false,
+        })
     }
 
     render() {
+        const actions = [
+            <FlatButton
+                label="Log In"
+                onClick={this.logIn}
+            />,
+            <FlatButton
+                label="Close"
+                onClick={this.close}
+            />,
+        ];
         return (
-                <div>
-                <TextField hintText="Username"/>
-                <br/>
-                <TextField hintText="Password"/>
-                </div>
+            <div>
+                <Dialog
+                    open={this.state.open}
+                    actions={actions}
+                    modal={true}
+                >
+                    <TextField hintText="Username" />
+                    <br />
+                    <TextField hintText="Password" />
+                </Dialog>
+            </div>
         );
     }
 }
