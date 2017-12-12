445485f40e20315d3eb30cc56bf62d321ded84f3
carlso70 <carlso70@purdue.edu>
2017-12-12 14:28:25 -0500

    fix url

--- webapp/src/utils/urls.js
+++ webapp/src/utils/urls.js
@@ -1,6 +1,6 @@
 export const loginUrl = "http://ec2-18-221-200-72.us-east-2.compute.amazonaws.com:8080/loginuser";
 export const createUserUrl = "http://ec2-18-221-200-72.us-east-2.compute.amazonaws.com:8080/createuser";
-export const getAllUsersUrl = "http://ec2-18-221-200-72.us-east-2.compute.amazonaws.com:8080/listUsers";
+export const getAllUsersUrl = "http://ec2-18-221-200-72.us-east-2.compute.amazonaws.com:8080/listusers";
 export const createGameUrl = "http://ec2-18-221-200-72.us-east-2.compute.amazonaws.com:8080/creategame";
 export const joinGameUrl = "http://ec2-18-221-200-72.us-east-2.compute.amazonaws.com:8080/joingame";
 export const highScoresUrl = "http://ec2-18-221-200-72.us-east-2.compute.amazonaws.com:8080/highscores";
