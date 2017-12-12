92e6409dd0ebdd525dc335103cfebab124386623
carlso70 <carlso70@purdue.edu>
2017-12-12 14:06:20 -0500

    Add urls to server

---
+++ webapp/src/utils/urls.js
@@ -0,0 +1,13 @@
+export const loginUrl = "http://ec2-18-221-200-72.us-east-2.compute.amazonaws.com:8080/loginuser";
+export const createUserUrl = "http://ec2-18-221-200-72.us-east-2.compute.amazonaws.com:8080/createuser";
+export const getAllUsersUrl = "http://ec2-18-221-200-72.us-east-2.compute.amazonaws.com:8080/listUsers";
+export const createGameUrl = "http://ec2-18-221-200-72.us-east-2.compute.amazonaws.com:8080/creategame";
+export const joinGameUrl = "http://ec2-18-221-200-72.us-east-2.compute.amazonaws.com:8080/joingame";
+export const highScoresUrl = "http://ec2-18-221-200-72.us-east-2.compute.amazonaws.com:8080/highscores";
+export const leaveGameUrl = "http://ec2-18-221-200-72.us-east-2.compute.amazonaws.com:8080/leavegame";
+export const setSecurityQuestionUrl = "http://ec2-18-221-200-72.us-east-2.compute.amazonaws.com:8080/setsecurityquestion";
+export const answerSecurityQuestionUrl = "http://ec2-18-221-200-72.us-east-2.compute.amazonaws.com:8080/answersecurityquestion";
+export const getSecurityQuestionUrl = "http://ec2-18-221-200-72.us-east-2.compute.amazonaws.com:8080/getsecurityquestion";
+
+
+
