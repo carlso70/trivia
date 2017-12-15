b7cf9ea9e4d1f46eff915bf72b4a8a468e9e96eb
James Carlson <carlso70@purdue.edu>
2017-12-14 23:57:19 -0500

    Create layout for home page

--- webapp/src/pages/Home.js
+++ webapp/src/pages/Home.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import FlatButton from 'material-ui/FlatButton';
 import RaisedButton from 'material-ui/RaisedButton';
 import Paper from 'material-ui/Paper';
+import MenuItem from 'material-ui/MenuItem';
+import SelectField from 'material-ui/SelectField';
 import {
     Table,
     TableBody,
@@ -31,26 +33,49 @@ const testTable = [
 const style = {
     margin: 15,
     padding: 10,
-    width: '75%',
-    display: 'inline-block',
 };
+
+const countItems = [];
+for (let i = 1; i < 20; i++ ) {
+    countItems.push(<MenuItem value={i} key={i} primaryText={`${i}`} />);
+}
+
 
 // Home Page contains the list of active, games and user stats
 class Home extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            showLogin: false
-        }
+            showLogin: false,
+            dif: 1,
+            count: 5
+        };
     }
+
+    handleDifChange = (event, index, value) => this.setState({dif: value});
+    handleCountChange = (event, index, value) => this.setState({count: value});
 
     render() {
         return (
-            <div>
+                <div>
                 <Paper style={style} zDepth={2}>
-                    <Slider defaultValue={20}/>
-                    <Slider defaultValue={3}/>
-                    <FlatButton >Create Game</FlatButton>
+                <h3>Game Settings</h3>
+                <SelectField onChange={this.handleDifChange} value={this.state.dif} floatingLabelText="Difficulty">
+                <MenuItem value={1} primaryText="Easy" />
+                <MenuItem value={2} primaryText="Medium" />
+                <MenuItem value={3} primaryText="Hard" />
+                </SelectField>
+                <br/>
+                <SelectField
+                 floatingLabelText="Question Count"
+                 value={this.state.count}
+                 onChange={this.handleCountChange}
+                 maxHeight={200}
+                  >
+                {countItems}
+                </SelectField>
+                <br />
+                <RaisedButton label="Create Game"/>
                 </Paper>
 
                 <Paper style={style} zDepth={2}>
