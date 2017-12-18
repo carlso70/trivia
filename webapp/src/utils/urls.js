4b0495a6d8be8d64a1747340c2006179c3fc5a0a
carlso70 <carlso70@purdue.edu>
2017-12-18 00:13:52 -0600

    adjust urls

--- webapp/src/utils/urls.js
+++ webapp/src/utils/urls.js
@@ -1,14 +1,13 @@
-export const loginUrl = "http://ec2-18-221-200-72.us-east-2.compute.amazonaws.com:8080/loginuser";
-export const createUserUrl = "http://ec2-18-221-200-72.us-east-2.compute.amazonaws.com:8080/createuser";
-export const getAllUsersUrl = "http://ec2-18-221-200-72.us-east-2.compute.amazonaws.com:8080/listusers";
-export const createGameUrl = "http://ec2-18-221-200-72.us-east-2.compute.amazonaws.com:8080/creategame";
-export const joinGameUrl = "http://ec2-18-221-200-72.us-east-2.compute.amazonaws.com:8080/joingame";
-export const startGameUrl = "http://ec2-18-221-200-72.us-east-2.compute.amazonaws.com:8080/startgame";
-export const highScoresUrl = "http://ec2-18-221-200-72.us-east-2.compute.amazonaws.com:8080/highscores";
-export const leaveGameUrl = "http://ec2-18-221-200-72.us-east-2.compute.amazonaws.com:8080/leavegame";
-export const listGames = "http://ec2-18-221-200-72.us-east-2.compute.amazonaws.com:8080/listgames";
-export const setSecurityQuestionUrl = "http://ec2-18-221-200-72.us-east-2.compute.amazonaws.com:8080/setsecurityquestion";
-export const answerSecurityQuestionUrl = "http://ec2-18-221-200-72.us-east-2.compute.amazonaws.com:8080/answersecurityquestion";
-export const getSecurityQuestionUrl = "http://ec2-18-221-200-72.us-east-2.compute.amazonaws.com:8080/getsecurityquestion";
-export const gameSocketUrl = "ws://ec2-18-221-200-72.us-east-2.compute.amazonaws.com:3000/game_socket/";
-
+export const loginUrl = "http://ec2-18-216-131-105.us-east-2.compute.amazonaws.com:8080/loginuser";
+export const createUserUrl = "http://ec2-18-216-131-105.us-east-2.compute.amazonaws.com:8080/createuser";
+export const getAllUsersUrl = "http://ec2-18-216-131-105.us-east-2.compute.amazonaws.com:8080/listusers";
+export const createGameUrl = "http://ec2-18-216-131-105.us-east-2.compute.amazonaws.com:8080/creategame";
+export const joinGameUrl = "http://ec2-18-216-131-105.us-east-2.compute.amazonaws.com:8080/joingame";
+export const startGameUrl = "http://ec2-18-216-131-105.us-east-2.compute.amazonaws.com:8080/startgame";
+export const highScoresUrl = "http://ec2-18-216-131-105.us-east-2.compute.amazonaws.com:8080/highscores";
+export const leaveGameUrl = "http://ec2-18-216-131-105.us-east-2.compute.amazonaws.com:8080/leavegame";
+export const listGames = "http://ec2-18-216-131-105.us-east-2.compute.amazonaws.com:8080/listgames";
+export const setSecurityQuestionUrl = "http://ec2-18-216-131-105.us-east-2.compute.amazonaws.com:8080/setsecurityquestion";
+export const answerSecurityQuestionUrl = "http://ec2-18-216-131-105.us-east-2.compute.amazonaws.com:8080/answersecurityquestion";
+export const getSecurityQuestionUrl = "http://ec2-18-216-131-105.us-east-2.compute.amazonaws.com:8080/getsecurityquestion";
+export const gameSocketUrl = "ws://ec2-18-216-131-105.us-east-2.compute.amazonaws.com:3000/game_socket/";
